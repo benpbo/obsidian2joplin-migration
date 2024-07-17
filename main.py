5a2ef8220636b5d69aae384e39731033751d0ce7
Ben Prize Bar-On <prizeben@gmail.com>
2024-07-17 17:41:09 +0200

    Use `joppy` module

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import requests
+from joppy.api import Api
 
 TOKEN = '2ac144116f437c87f2973dd6d727cbc7e313d6109061ce2b7472eba7898d113d507440f52260a223da12c82f22b2f1e64e316dc5fe074a691c4b41476c277c66'
 
-def main():
-    r = requests.get(f'http://localhost:41184/notes', params={'token': TOKEN})
-    print(r.json())
+def main(api: Api):
+    print(api.get_all_notes())
 
 if __name__ == '__main__':
-    main()
+    api = Api(token=TOKEN)
+    main(api)
